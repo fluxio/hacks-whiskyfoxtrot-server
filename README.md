b90301077463be143ff05737421d597a995e1297
Keunwoo Lee <keunwoo@flux.io>
2016-12-09 20:37:39 -0500

    get bare-bones login working

--- README.md
+++ README.md
@@ -1,39 +1,21 @@
-# node-js-getting-started
+Create the following .env:
 
-A barebones Node.js app using [Express 4](http://expressjs.com/).
+```
+FLUX_CLIENT_ID=client_id_here
+FLUX_CLIENT_SECRET=client_secret_here
+FLUX_REDIRECT_URI=http://localhost:5000/oauthcb
+PORT=5000
+SESSION_SECRET=session_secret_here
+```
 
-This application supports the [Getting Started with Node on Heroku](https://devcenter.heroku.com/articles/getting-started-with-nodejs) article - check it out.
+Replace client_id_here and client_secret_here with your Flux client
+ID/secret.
+
+Replace session_secret_here with some arbitrary random string.
 
 ## Running Locally
 
-Make sure you have [Node.js](http://nodejs.org/) and the [Heroku Toolbelt](https://toolbelt.heroku.com/) installed.
-
 ```sh
-$ git clone git@github.com:heroku/node-js-getting-started.git # or clone your own fork
-$ cd node-js-getting-started
 $ npm install
-$ npm start
+$ heroku local
 ```
-
-Your app should now be running on [localhost:5000](http://localhost:5000/).
-
-## Deploying to Heroku
-
-```
-$ heroku create
-$ git push heroku master
-$ heroku open
-```
-or
-
-[![Deploy to Heroku](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
-
-## Documentation
-
-For more information about using Node.js on Heroku, see these Dev Center articles:
-
-- [Getting Started with Node.js on Heroku](https://devcenter.heroku.com/articles/getting-started-with-nodejs)
-- [Heroku Node.js Support](https://devcenter.heroku.com/articles/nodejs-support)
-- [Node.js on Heroku](https://devcenter.heroku.com/categories/nodejs)
-- [Best Practices for Node.js Development](https://devcenter.heroku.com/articles/node-best-practices)
-- [Using WebSockets on Heroku with Node.js](https://devcenter.heroku.com/articles/node-websockets)
--- index.js
+++ index.js
@@ -1,20 +1,109 @@
+// nodejs standard libraries
+var crypto = require('crypto');
+var process = require('process');
+
+// express
+var bodyParser = require('body-parser');
 var express = require('express');
+var session = require('express-session');
+
+// other npm libraries
+var fluxSDK = require('flux-sdk-node');
+
+var config = {
+    debug: process.env.NODE_ENV !== 'production',
+    fluxClientId: process.env.FLUX_CLIENT_ID,
+    fluxClientSecret: process.env.FLUX_CLIENT_SECRET,
+    fluxRedirectUri: process.env.FLUX_REDIRECT_URI,
+    fluxUrl: process.env.FLUX_URL,
+    port: process.env.PORT,
+    sessionSecret: process.env.SESSION_SECRET
+};
+
+var sdk = new fluxSDK(config.fluxClientId, {
+    fluxUrl: config.fluxUrl,
+    clientSecret: config.fluxClientSecret,
+    redirectUri: config.fluxRedirectUri
+});
+
 var app = express();
+app.set('port', config.port);
+app.set('x-powered-by', false);
 
-app.set('port', (process.env.PORT || 5000));
-
+// setup template file search configuration
 app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.use(bodyParser.json());
+
+app.use(session({
+    secret: config.sessionSecret,
+    saveUninitialized: true,
+    resave: true
+    // TODO(keunwoo): add a session store that survives restarts
+}));
+
+app.get('/', function(req, res, next) {
+    var idToken = req && req.session && req.session.fluxCredentials && req.session.fluxCredentials.idToken;
+    console.log('idToken: ' + idToken);
+    res.render('pages/index', {
+        userId: idToken && idToken.payload && idToken.payload.flux_id_hash
+    });
+});
+
+var onInternalErr = function(res, err) {
+    console.log('internal error: ' + err);
+    res.status(500).send('internal error');
+};
+
+app.use('/oauth', function(req, res, next) {
+    crypto.randomBytes(24, function(err, nonceBytes) {
+        if (err) {
+            onInternalErr(res, err);
+            return;
+        }
+
+        crypto.randomBytes(24, function(err, stateSeedBytes) {
+            if (err) {
+                onInternalErr(res, err);
+                return;
+            }
+            req.session.nonce = nonceBytes.toString('hex');
+            // TODO(keunwoo): cryptographically bind state to current session.
+            req.session.state = stateSeedBytes.toString('hex');
+            var authorizeUrl = sdk.getAuthorizeUrl(req.session.state, req.session.nonce);
+            res.redirect(authorizeUrl);
+        });
+    });
+});
+
+app.use('/oauthcb', function(req, res, next) {
+    var reqState;
+    if (req.method === 'GET') {
+        reqState = req.query.state;
+    } else if (req.method === 'POST') {
+        reqState = req.body.state;
+    } else {
+        res.status(405).send('invalid request method');
+        return;
+    }
+
+    if (req.session.state !== reqState) {
+        res.status(400).send('invalid oauth state');
+        return;
+    }
+
+    sdk.exchangeCredentials(req.session.state, req.session.nonce, req.query)
+        .then(function(credentials) {
+            console.log('stashing credentials in session:');
+            console.log(credentials);
+            req.session.fluxCredentials = credentials;
+            res.redirect('/');
+        })
+        .catch(next);
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
-
-
